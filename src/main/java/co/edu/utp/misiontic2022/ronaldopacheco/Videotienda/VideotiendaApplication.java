501d67e2bf947b645dcc62e3b9eec755fb2f273d
Ronaldo Pacheco <rony8546@gmail.com>
2022-09-24 21:42:24 -0500

    proyecto videotienda

---
+++ src/main/java/co/edu/utp/misiontic2022/ronaldopacheco/Videotienda/VideotiendaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.ronaldopacheco.Videotienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideotiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/utp/misiontic2022/ronaldopacheco/Videotienda/VideotiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.ronaldopacheco.Videotienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
